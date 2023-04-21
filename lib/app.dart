a5bd3d350982d743ca8679cad751e65796d6be33
BahromjonPolat <itjunior.uz@gmail.com>
2023-04-21 20:47:34 +0500

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,25 @@
+/*
+
+  Created by: Bakhromjon Polat
+  Created on: Apr 21 2023 20:39:00
+  Github:   https://github.com/BahromjonPolat
+  Leetcode: https://leetcode.com/BahromjonPolat/
+  LinkedIn: https://linkedin.com/in/bahromjon-polat
+  Telegram: https://t.me/BahromjonPolat
+
+  Documentation: 
+
+*/
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'TODO',
+      theme: ThemeData(),
+    );
+  }
+}
